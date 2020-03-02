284729c71e5594e104e333810c6977f6813e19aa
bmark88 <bradley.mark@hotmail.com>
2020-03-02 20:31:00 +0000

    added tail function

---
+++ tail.js
@@ -0,0 +1,32 @@
+// FUNCTION IMPLEMENTATION WITH TEMPLATE LITERALS
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    // console.assert(actual === expected, "🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+const tail = (givenArray) => {
+  let tailArray = [];
+
+  for (let i = 1; i < givenArray.length; i++) {
+    tailArray.push(givenArray[i]);
+  }
+
+  return tailArray;
+};
+
+//TEST CODE
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
