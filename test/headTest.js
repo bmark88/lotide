a3e7e1c68e72ef0ff67efd19ba07daa915df395f
bmark88 <bradley.mark@hotmail.com>
2020-03-09 17:30:50 +0000

    added test file for tail.js and refactoreed tail.js

--- test/headTest.js
+++ test/headTest.js
@@ -4,5 +4,3 @@ const head = require('../head');
 console.log(head([5,6,7]));
 console.log(head(["Hello", "Lighthouse", "Labs"]));
 console.log(head([]));
-
-module.exports = head;
---
+++ test/tailTest.js
@@ -0,0 +1,11 @@
+const tail = require('../tail');
+const assertEqual = require('../assertEqual')
+
+// TEST CODE
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
