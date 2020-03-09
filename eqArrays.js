16ffb2b20a58d1e70ae62e25895a1151f656a08f
bmark88 <bradley.mark@hotmail.com>
2020-03-09 17:39:29 +0000

    added tests for eqArrays and refactored eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -12,9 +12,4 @@ const eqArrays = (actual, expected) => {
   return true;
 };
 
-//TEST CODE
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+module.exports = eqArrays
